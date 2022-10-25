a9c73406f622445867c60e5227f41f7f94d9c044
CS Goh <cs.goh@outlook.com>
2022-10-25 21:51:10 +1300

    Updated to show item text within the bar

--- MahereKaihanga.py
+++ MahereKaihanga.py
@@ -1,4 +1,4 @@
-# RoadMapGenerator
+# Mahere Kaihanga / Roadmap Generator
 # Copyright (C) 2022 Cheng Soon Goh
 #
 # This program is free software: you can redistribute it and/or modify it under
@@ -19,7 +19,7 @@ from webcolors import name_to_rgb
 from dateutil.relativedelta import relativedelta
 
 
-class RoadmapGenerator():
+class MahereKaihanga():
     # Default settings
     Width, Height = 1024, 512 
     VSPACER, HSPACER = 12, 2
@@ -46,7 +46,7 @@ class RoadmapGenerator():
         return [x / 255 for x in fRGBS]
         #return fRGBS[0] / 255, fRGBS[1] / 255, fRGBS[2] / 255
 
-    def render(self):
+    def render(self, fileName):
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.Width, self.Height)
         cr = cairo.Context(surface)
         
@@ -92,11 +92,10 @@ class RoadmapGenerator():
         # Determine max task text width
         taskWidth = 0
         for x in taskData:
-            taskText = x.get("task")
-            textXbearing, textYbearing, textWidth, textHeight, dx, dy = cr.text_extents(taskText)
-            #print (">" + x + ":" + str(textWidth))
-            if textWidth > taskWidth:
-                taskWidth = textWidth + 100
+            groupText = x.get("group")
+            groupTextXbearing, groupTextYbearing, groupTextWidth, groupTextHeight, dx, dy = cr.text_extents(groupText)
+            if groupTextWidth > taskWidth:
+                taskWidth = groupTextWidth 
 
         #print (taskWidth)
         timelineWidth = self.Width - taskWidth - (self.HSPACER * self.TimelineItem) - 10
@@ -143,9 +142,9 @@ class RoadmapGenerator():
                 lastGroupText = groupText
                 
                 cr.set_source_rgb(tltcR, tltcG, tltcB)
-                textXbearing, textYbearing, groupWidth, groupHeight, dx, dy = cr.text_extents(groupText)
+                groupTextXbearing, groupTextYbearing, groupTextWidth, groupTextHeight, dx, dy = cr.text_extents(groupText)
                 if (nextGroupY == 0):
-                    groupY = groupYPos + (groupHeight * i) + (self.VSPACER * i) 
+                    groupY = groupYPos + (groupTextHeight * i) + (self.VSPACER * i) 
                 else:
                     groupY = nextGroupY + 30
 
@@ -159,84 +158,97 @@ class RoadmapGenerator():
                 nextGroupY = taskYPos 
 
                 j = 0
-                taskX = groupWidth 
+                taskX = groupTextWidth 
                 for y in taskData:
                     # Draw task item
-                    #print (">", groupText, y.get("group"))
                     if (groupText == y.get("group")):
                         
-                        taskText = y.get("task")
-                        #print (">>>>", taskText)
-                        
-                        cr.set_source_rgb(tltcR, tltcG, tltcB)
-                        textXbearing, textYbearing, textWidth, textHeight, dx, dy = cr.text_extents(taskText)
-                        yPos = taskYPos + textHeight * j + (self.VSPACER * j)
-                       
-                        cr.move_to(taskX, yPos)
-                        
-                        cr.show_text(taskText)
-                        nextGroupY = yPos
-                        #print (">nextGroupY", nextGroupY)
+                        tasks = y.get("tasks")
+                        for task in tasks:
+                            taskText = task.get("task")
+                            #print (">>>>", taskText)
+                            
+                            cr.set_source_rgb(tltcR, tltcG, tltcB)
+                            textXbearing, textYbearing, textWidth, textHeight, dx, dy = cr.text_extents(taskText)
+                            yPos = taskYPos + textHeight * j + (self.VSPACER * j)
+                            nextGroupY = yPos
 
-                        # Draw task bar
-                        taskR, taskG, taskB = self.rgb_to_float(x.get("colour"))
+                            # Draw task bar
+                            taskR, taskG, taskB = self.rgb_to_float(task.get("colour"))
+                            row = 0
+                            
+                            taskStartMonth = task.get("start").month
+                            taskEndMonth = task.get("end").month
+                            taskStartDate = datetime.datetime(task.get("start").year, taskStartMonth, 1)
+                            taskEndDate = datetime.datetime(task.get("end").year, taskEndMonth, 1)
+                            
+                            totalBarWidth = 0
+                            startPos = 0
+                            rowMatch = 0
+                            for z in range(self.TimelineItem):
+                                thisMonth = (self.Today + relativedelta(months=+z)).month
+                                thisYear = (self.Today + relativedelta(months=+z)).year
+                                thisDate = datetime.datetime(thisYear, thisMonth, 1)
 
-                        #print(taskText, "Task date: " + str(taskData[i]["start"].month) + " - " + str(taskData[i]["end"].month))
-                        row = 0
-                        
-                        taskStartMonth = y.get("start").month
-                        taskEndMonth = y.get("end").month
-                        taskStartDate = datetime.datetime(y.get("start").year, taskStartMonth, 1)
-                        taskEndDate = datetime.datetime(y.get("end").year, taskEndMonth, 1)
-                        #print ("task : ", j, taskText, taskStartDate, taskEndDate)
-
-                        for z in range(self.TimelineItem):
-                            thisMonth = (self.Today + relativedelta(months=+z)).month
-                            thisYear = (self.Today + relativedelta(months=+z)).year
-                            thisDate = datetime.datetime(thisYear, thisMonth, 1)
-
-                            if (taskStartDate <= thisDate and taskEndDate >= thisDate):
-                                #print("     Task Position: ", taskText, str(taskX), str(yPos))
-                                cr.set_source_rgb(taskR, taskG, taskB)
-                                if (row == (self.TimelineItem - 1)):
-                                    #print (row, timelineItem)
-                                    cr.rectangle(timelinePositions[z][0],yPos-15, timelinePositions[z][2], textHeight+5)
-                                else:
-                                    cr.rectangle(timelinePositions[z][0],yPos-15, timelinePositions[z][2]+self.HSPACER+1, textHeight+5)
-                                cr.fill()    
-                            row += 1
-                        j += 1
+                                if (taskStartDate <= thisDate and taskEndDate >= thisDate):
+                                    cr.set_source_rgb(taskR, taskG, taskB)
+                                    if (row == (self.TimelineItem - 1)):
+                                        cr.rectangle(timelinePositions[z][0],yPos-15, timelinePositions[z][2], textHeight+5)
+                                    else:
+                                        cr.rectangle(timelinePositions[z][0],yPos-15, timelinePositions[z][2]+self.HSPACER+1, textHeight+5)
+                                    cr.fill()   
+                                    if (rowMatch == 0):
+                                        startPos = timelinePositions[z][0]
+                                    rowMatch += 1
+                                row += 1
+                            totalBarWidth = timelineItemWidth * rowMatch
+                            cr.set_source_rgb(0,0,1)
+                            barTextX = startPos + (totalBarWidth / 2) - (textWidth / 2)
+                            barTextY = yPos-3
+                            cr.move_to(barTextX,barTextY)
+                            cr.set_source_rgb(0,0,1)
+                            cr.show_text(taskText)
+                            j += 1
                     
                 i += 1
 
         """ To do
-        (1) Display task text on top of bar
+        Done (1) Display task text on top of bar
         (2) Display group text as a block
         (3) Show milestones
         (4) Proporsion task bar according to the start and end date
         """
-        footerText = "Powered by RoadMapGenerator"
+        footerText = "Powered by Mahere Kaihanga v0.1"
         cr.set_source_rgb(footerR, footerG, footerB)
         cr.set_font_size(10)
         footerXbearing, footerYbearing, footerWidth, footerHeight, dx, dy = cr.text_extents(footerText)
         cr.move_to((self.Width/2) - footerWidth/2, self.Height - 10)
         cr.show_text(footerText)
 
-        surface.write_to_png("example.png")  # Output to PNG
+        if (len(fileName) == 0):
+            fileName = "roadmap.png"
+        surface.write_to_png(fileName)  # Output to PNG
 
 
 if __name__ == "__main__":
-    x = RoadmapGenerator()
+    x = MahereKaihanga()
     x.Title = "This is my roadmap"
+
     x.Tasks = [
-                {"group": "Sample Group 1", "task": "Feature 1", "start": datetime.datetime(2022, 10, 24), "end": datetime.datetime(2022, 11, 24), "colour": "lightgreen"},
-                {"group": "Sample Group 1", "task": "Feature 2", "start": datetime.datetime(2022, 12, 24), "end": datetime.datetime(2023, 4, 24), "colour": "lightgreen"},
-                {"group": "Sample Group 2", "task": "Feature 3", "start": datetime.datetime(2022, 4, 24), "end": datetime.datetime(2022, 12, 24), "colour": "lightblue"},
-                {"group": "Sample Group 2", "task": "Feature 4", "start": datetime.datetime(2023, 1, 24), "end": datetime.datetime(2024, 12, 24), "colour": "lightblue"},
-                {"group": "Sample Group 3", "task": "Feature 5", "start": datetime.datetime(2022, 10, 24), "end": datetime.datetime(2023, 3, 24), "colour": "yellow"},
-                {"group": "Sample Group 3", "task": "Feature 6", "start": datetime.datetime(2023, 4, 24), "end": datetime.datetime(2023, 7, 24), "colour": "yellow"},
-              
+                {"group": "Group 1", "colour": "lightgreen", "tasks": [
+                    {"task": "Feature 1", "start": datetime.datetime(2022, 10, 24), "end": datetime.datetime(2022, 11, 24), "colour": "lightgreen"},
+                    {"task": "Feature 2", "start": datetime.datetime(2022, 12, 24), "end": datetime.datetime(2023, 4, 24), "colour": "lightgreen"}
+                    ]},
+                {"group": "Group 2", "colour": "lightgreen", "tasks": [
+                    {"task": "Feature 3", "start": datetime.datetime(2022, 4, 24), "end": datetime.datetime(2022, 12, 24), "colour": "lightblue"},
+                    {"task": "Feature 4", "start": datetime.datetime(2023, 1, 24), "end": datetime.datetime(2024, 12, 24), "colour": "lightblue"}
+                    ]},
+                {"group": "Group 3", "colour": "lightgreen", "tasks": [
+                    {"task": "Feature 5", "start": datetime.datetime(2022, 10, 24), "end": datetime.datetime(2023, 3, 24), "colour": "yellow"},
+                    {"task": "Feature 6", "start": datetime.datetime(2023, 4, 24), "end": datetime.datetime(2023, 7, 24), "colour": "yellow"},
+                    {"task": "Feature 7", "start": datetime.datetime(2023, 8, 24), "end": datetime.datetime(2023, 8, 24), "colour": "yellow"}
+                ]}              
             ]
-    x.render()
-    #print(x.rgb_to_float("Pink"))
+    x.render("my_roadmap.png")
+
 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # roadmap_generator
 
 Purpose: This code is used to generate graphical roadmap using code. 
+
+Mahere Kaihanga means roadmap generator in Maori language.
