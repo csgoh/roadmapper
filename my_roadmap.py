121aef84edc71f7b8bd9419b7e7eeaf675138fce
CS Goh <cs.goh@outlook.com>
2022-11-06 15:24:27 +1300

    implemented milestone markers

--- my_roadmap.py
+++ my_roadmap.py
@@ -3,10 +3,11 @@ from generator import Roadmap
 my_roadmap = Roadmap(1400, 612, "my_roadmap.png")
 
 my_roadmap.title_text = "This is my roadmap!!!"
+my_roadmap.timeline_start_date = "2022-12-01"
 # my_roadmap.timeline_fill_colour = "#527a7a"
 # my_roadmap.timeline_text_colour = "White"
-my_roadmap.timeline_mode = Roadmap.YEARLY
-my_roadmap.timeline_item = 6
+my_roadmap.timeline_mode = Roadmap.WEEKLY
+my_roadmap.timeline_item = 12
 # my_roadmap.group_text_colour = "Black"
 # my_roadmap.task_text_colour = "Black"
 my_roadmap.show_footer = True
@@ -15,12 +16,12 @@ group1 = my_roadmap.add_group("Stream 1: Develop base")
 task1 = my_roadmap.add_task(group1, "Feature 1", "2022-10-24", "2022-11-24")
 task2 = my_roadmap.add_task(group1, "Feature 2", "2022-12-24", "2023-04-24")
 my_roadmap.add_milestone(group1, task1, "v1.0 MVP", "2022-11-01")
-my_roadmap.add_milestone(group1, task2, "v1.1", "2023-04-24")
-my_roadmap.add_milestone(group1, task2, "v1.2", "2023-12-15")
+my_roadmap.add_milestone(group1, task2, "v1.1", "2023-01-24")
+my_roadmap.add_milestone(group1, task2, "v1.2", "2023-04-15")
 
 group2 = my_roadmap.add_group("Stream 2: Enable monitoring")
 task3 = my_roadmap.add_task(group2, "Feature 3", "2022-04-24", "2023-01-24")
-my_roadmap.add_milestone(group2, task3, "Monitoring Portal 1", "2022-12-30")
+my_roadmap.add_milestone(group2, task3, "Monitoring Portal 1", "2023-01-24")
 
 task4 = my_roadmap.add_task(group2, "Feature 4", "2023-01-24", "2024-12-24")
 my_roadmap.add_milestone(group2, task4, "Monitoring Portal 2", "2023-06-30")
