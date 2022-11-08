1ffade6c5660e61cf4af23f91111488f9fb7027a
CS Goh <cs.goh@outlook.com>
2022-11-09 11:03:27 +1300

    nested contextmanager example

--- sandpit5.py
+++ sandpit5.py
@@ -1,4 +1,47 @@
-from datetime import datetime
+from contextlib import contextmanager
 
-print(datetime.strftime(datetime.today(), "%Y-%m-%d"))
-print(datetime.strptime(datetime.strftime(datetime.today(), "%Y-%m-%d"), "%Y-%m-%d"))
+class Parent:
+   
+    @contextmanager
+    def do_something(self):
+        try:
+            print(type(self).__name__, id(self), "I'm doing something")
+            yield Child()
+        finally:
+            print(type(self).__name__, id(self),"I'm done!")
+            
+class Child:
+    @contextmanager
+    def do_something(self):
+        try:
+            print(type(self).__name__, id(self), "    I'm doing something")
+            yield GrandChild()
+        finally:
+            print(type(self).__name__, id(self), "    I'm done!")
+
+    
+class GrandChild:
+    @contextmanager
+    def do_something(self):
+        print(type(self).__name__, id(self), "        I'm doing something")
+        print(type(self).__name__, id(self), "        I'm done!")
+
+
+my_parent = Parent()
+
+with my_parent.do_something() as my_child:
+    with my_child.do_something() as my_grandchild:
+        my_grandchild.do_something()
+        my_grandchild.do_something()
+    with my_child.do_something() as my_grandchild:
+        my_grandchild.do_something()
+        my_grandchild.do_something()
+        
+print ("*********")
+with my_parent.do_something() as my_child:
+    with my_child.do_something() as my_grandchild:
+        my_grandchild.do_something()
+        my_grandchild.do_something()
+    with my_child.do_something() as my_grandchild:
+        my_grandchild.do_something()
+        my_grandchild.do_something()
