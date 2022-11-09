1e37ca6cfb84b57526e0200444ae5b8bfb880c6f
CS Goh <cs.goh@outlook.com>
2022-11-09 16:43:46 +1300

    add sandpit5

--- sandpit5.py
+++ sandpit5.py
@@ -1,47 +1,66 @@
 from contextlib import contextmanager
+import uuid
 
-class Parent:
-   
-    @contextmanager
-    def do_something(self):
-        try:
-            print(type(self).__name__, id(self), "I'm doing something")
-            yield Child()
-        finally:
-            print(type(self).__name__, id(self),"I'm done!")
-            
-class Child:
-    @contextmanager
-    def do_something(self):
-        try:
-            print(type(self).__name__, id(self), "    I'm doing something")
-            yield GrandChild()
-        finally:
-            print(type(self).__name__, id(self), "    I'm done!")
-
-    
-class GrandChild:
-    @contextmanager
-    def do_something(self):
-        print(type(self).__name__, id(self), "        I'm doing something")
-        print(type(self).__name__, id(self), "        I'm done!")
-
-
-my_parent = Parent()
-
-with my_parent.do_something() as my_child:
-    with my_child.do_something() as my_grandchild:
-        my_grandchild.do_something()
-        my_grandchild.do_something()
-    with my_child.do_something() as my_grandchild:
-        my_grandchild.do_something()
-        my_grandchild.do_something()
+class Group:
+    tasks = []
+    def __init__(self) -> None:
+        self.uid = uuid.uuid4()
         
+    @contextmanager
+    def do_something(self):
+        try:
+            print(type(self).__name__, self.uid, "I'm doing something")
+            yield Group()
+        finally:
+            print(type(self).__name__, self.uid,"I'm done!")
+    def add_task(self, task):
+        self.tasks.append(task)
+    
+            
+class Task:
+    milestones = []
+    def __init__(self) -> None:
+        self.uid = uuid.uuid4()
+    
+    @contextmanager
+    def do_something(self):
+        try:
+
+            print("     ", type(self).__name__, self.uid, "I'm doing something")
+            yield Task()
+        finally:
+            print("     ", type(self).__name__, self.uid, "I'm done!")
+    
+    def add_milestone(self, milestone):
+        self.milestones.append(milestone)
+    
+class Milestone:
+    def __init__(self) -> None:
+        self.uid = uuid.uuid4()
+        
+    @contextmanager
+    def do_something(self):
+        
+        print("             ", type(self).__name__, self.uid, "I'm doing something")
+        print("             ", type(self).__name__, self.uid, "I'm done!")
+
+my_group1 = Group()
+
+with my_group1.do_something() as group:
+    with group.do_something() as task:
+        pass
+    with group.do_something() as task:
+        task.do_something()
+        group.do_something()
+    with group.do_something() as task:
+        task.do_something()
+        task.do_something()
 print ("*********")
-with my_parent.do_something() as my_child:
-    with my_child.do_something() as my_grandchild:
-        my_grandchild.do_something()
-        my_grandchild.do_something()
-    with my_child.do_something() as my_grandchild:
-        my_grandchild.do_something()
-        my_grandchild.do_something()
+my_group2 = Group()
+with my_group2.do_something() as group:
+    with group.do_something() as task:
+        task.do_something()
+        task.do_something()
+    with group.do_something() as task:
+        task.do_something()
+        task.do_something()
