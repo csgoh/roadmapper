2da19aaedca8250be0d1c40e6e426e041db6267b
CS Goh <cs.goh@outlook.com>
2023-05-26 23:36:20 +1200

    renamed to test_generate_roadmap.py

--- src/tests/test_generate_roadmap.py
+++ src/tests/test_generate_roadmap.py
@@ -6,5 +6,6 @@ import src.tests.roadmap_generators.roadmap_generator as roadmap_generator
 
 @pytest.mark.genimage
 class TestGenerateRoadmaps:
-    roadmap_generator.generate_and_save_all_roadmaps_in()
-    assert True
+    def test_generate_and_save_all_roadmaps_in(self):
+        roadmap_generator.generate_and_save_all_roadmaps_in()
+        assert True
