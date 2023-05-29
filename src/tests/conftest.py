60b2d3faf649bb9050395e6b952e0d15a23aceb5
Felix Seifert <mail@felix-seifert.com>
2023-05-29 14:32:15 +0200

    Enable OS agnostic tests with OS var in tests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.fixture(autouse=True)
 def change_test_dir(request, monkeypatch):
     monkeypatch.chdir(request.fspath.dirname)
+
+
+@pytest.fixture(scope="session")
+def operating_system_ubuntu():
+    return "Ubuntu"
