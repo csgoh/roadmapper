16eff47f2dac477c88f855685bd0d92d963fe007
Felix Seifert <mail@felix-seifert.com>
2023-05-29 14:34:08 +0200

    Comment pytest fixtures

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,9 +3,16 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def change_test_dir(request, monkeypatch):
+    """
+    This fixture ensures that generated roadmaps end up in correct path,
+    no matter from which path tests are executed.
+    """
     monkeypatch.chdir(request.fspath.dirname)
 
 
 @pytest.fixture(scope="session")
 def operating_system_ubuntu():
+    """
+    This fixture can be used as test argument to provide string of used OS `Ubuntu`.
+    """
     return "Ubuntu"
