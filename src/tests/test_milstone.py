8a61bdf67d73b2067b1532bb00cd7aff9eaa7e71
Patrick Shechet <patrick.shechet@gmail.com>
2023-11-11 13:24:02 -0700

    Mark new unit tests for milestone

--- src/tests/test_milstone.py
+++ src/tests/test_milstone.py
@@ -35,6 +35,7 @@ class MockPainter:
         return (100, 100)
 
 
+@pytest.mark.unit
 @pytest.fixture(scope="function")
 def milestone():
     milestone = Milestone(
@@ -53,6 +54,7 @@ def milestone():
     return milestone
 
 
+@pytest.mark.unit
 @pytest.fixture(scope="function")
 def painter():
     return MockPainter()
@@ -67,13 +69,10 @@ def test_draw_milestone_center_alignment(milestone, painter, text_alignment):
     assert painter.last_text == ("text", 200, 200, "Test", "Arial", 12, "black")
 
 
+@pytest.mark.unit
 @pytest.mark.parametrize(
     "text_alignment, expected_x",
-    [
-        ("left", 150),
-        ("Left:199",1),
-        ("left:90%", 110)
-    ],
+    [("left", 150), ("Left:199", 1), ("left:90%", 110)],
 )
 def test_draw_milestone_left_alignment(milestone, painter, text_alignment, expected_x):
     milestone.text_alignment = text_alignment
@@ -81,13 +80,11 @@ def test_draw_milestone_left_alignment(milestone, painter, text_alignment, expec
     assert painter.text_count > 0
     assert painter.last_text == ("text", expected_x, 200, "Test", "Arial", 12, "black")
 
+
+@pytest.mark.unit
 @pytest.mark.parametrize(
     "text_alignment, expected_x",
-    [
-        ("right", 250),
-        ("Right:1",201),
-        ("right:90%", 290)
-    ],
+    [("right", 250), ("Right:1", 201), ("right:90%", 290)],
 )
 def test_draw_milestone_right_alignment(milestone, painter, text_alignment, expected_x):
     milestone.text_alignment = text_alignment
@@ -96,7 +93,7 @@ def test_draw_milestone_right_alignment(milestone, painter, text_alignment, expe
     assert painter.last_text == ("text", expected_x, 200, "Test", "Arial", 12, "black")
 
 
-
+@pytest.mark.unit
 @pytest.mark.parametrize("invalid_alignment", ["top", "bottom", "diagonal"])
 def test_invalid_text_alignment(milestone, invalid_alignment):
     milestone.text_alignment = invalid_alignment
