53d49f881dda584628a39cb5916861d96510fa21
Felix Seifert <mail@felix-seifert.com>
2024-03-16 10:45:07 +0100

    Update (base update)
    [ghstack-poisoned]

---
+++ src/roadmapper/timelinelocale.py
@@ -0,0 +1,121 @@
+# MIT License
+
+# Copyright (c) 2022 CS Goh
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from dataclasses import dataclass
+import json
+import os
+import locale
+
+default_timeline_locale_settings = {
+    "locale": "en_US",
+    "settings": {
+        "year": {
+            "text": "Year {0}",
+            "generic_text": "Year {0}",
+        },
+        "half_year": {"text": "H{0}"},
+        "quarter": {"text": "Q{0}"},
+        "month": {
+            "text": "{0}",
+            "generic_text": "Month {0}",
+        },
+        "week": {
+            "text": "{0} {1}",
+            "generic_text": "W{0}",
+        },
+    },
+}
+
+### v1.1.1 Add generic timeline locale settings
+generic_timeline_locale_settings = {
+    "locale": "",
+    "settings": {
+        "year": {
+            "text": "Year {0}",
+            "generic_text": "Year {0}",
+        },
+        "half_year": {"text": "H{0}"},
+        "quarter": {"text": "Q{0}"},
+        "month": {
+            "text": "{0}",
+            "generic_text": "Month {0}",
+        },
+        "week": {
+            "text": "{0} {1}",
+            "generic_text": "W{0}",
+        },
+    },
+}
+
+
+TimelineLocaleSettings = [
+    default_timeline_locale_settings,
+    ### v1.1.1 Add generic timeline locale settings
+    generic_timeline_locale_settings,
+    ### Add more themes here
+]
+
+
+@dataclass
+class TimelineLocale:
+    """Timeline locale for the Roadmapper."""
+
+    def __init__(self, locale_name: str) -> None:
+        """Initialise the locale settings."""
+
+        # check if colour_theme_name is a json file
+        if locale_name.endswith(".json"):
+            if not os.path.isfile(locale_name):
+                raise ValueError(f"Locale {locale_name} not recognised.")
+
+            with open(locale_name, "r", encoding="utf8") as f:
+                timeline_locale_json = json.load(f)
+            if "locale" not in timeline_locale_json:
+                raise ValueError(f"Locale {locale_name} not recognised.")
+            locale_name = timeline_locale_json["locale"]
+            TimelineLocaleSettings.append(timeline_locale_json)
+        self._timeline_locale_name = locale_name
+        locale.setlocale(locale.LC_ALL, locale_name)
+
+    def get_timeline_locale_settings(self, timeline_mode: str) -> tuple:
+        """ "Get the timeline locale settings for the specified timeline mode.
+
+        Args:
+            timeline_mode (str): Timeline mode component to get the corresponding display settings.
+                                        Components are: "year", "half-year", "quarter", "month", "week".
+
+        Returns:
+            tuple: Tuple of the display settings for the specified timeline mode component.
+        """
+        locale_settings = None
+
+        for value in TimelineLocaleSettings:
+            if value["locale"] == self._timeline_locale_name:
+                locale_settings = value["settings"]
+                break
+
+        ### get the colour scheme for the specified roadmap component
+        ### values() returns a list of dictionaries, convert it to tuple. e.g. {1, 2} -> (1, 2)
+        if len(locale_settings[timeline_mode].values()) > 1:
+            return tuple(locale_settings[timeline_mode].values())
+        else:
+            return tuple(locale_settings[timeline_mode].values())[0]
---
+++ src/tests/test_alignment.py
@@ -0,0 +1,122 @@
+import pytest
+
+from src.roadmapper.alignment import Alignment, AlignmentDirection, OffsetType
+
+
+@pytest.mark.unit
+def test_case_insensitivity():
+    assert AlignmentDirection["center"] == AlignmentDirection.CENTER
+    assert AlignmentDirection["LEFT"] == AlignmentDirection.LEFT
+    assert OffsetType["unit"] == OffsetType.UNIT
+
+
+@pytest.mark.unit
+def test_enum_direction_synonyms():
+    assert AlignmentDirection.CENTRE == AlignmentDirection.CENTER
+
+
+@pytest.mark.unit
+def test_alignment_from_string():
+    alignment = Alignment.from_value("left:50%")
+    assert alignment.direction == AlignmentDirection.LEFT
+    assert alignment.offset_type == OffsetType.PERCENT
+    assert alignment.offset == 0.5
+
+
+@pytest.mark.unit
+def test_from_string():
+    alignment = Alignment.from_string("right:30%")
+    assert alignment.direction == AlignmentDirection.RIGHT
+    assert alignment.offset_type == OffsetType.PERCENT
+    assert alignment.offset == 0.3
+
+
+@pytest.mark.unit
+def test_from_alignment():
+    alignment = Alignment(direction=AlignmentDirection.LEFT)
+    new_alignment = Alignment.from_alignment(alignment)
+    assert new_alignment.direction == AlignmentDirection.LEFT
+
+
+@pytest.mark.unit
+def test_parse_offset():
+    offset, offset_type = Alignment.parse_offset("50%")
+    assert offset == 0.5
+    assert offset_type == OffsetType.PERCENT
+
+    offset, offset_type = Alignment.parse_offset("30")
+    assert offset == 30
+    assert offset_type == OffsetType.UNIT
+
+
+@pytest.mark.unit
+def test_update_from_alignment_string():
+    alignment = Alignment()
+    alignment.update_from_alignment_string("centre:20%")
+    assert alignment.direction == AlignmentDirection.CENTRE
+    assert alignment.offset_type == OffsetType.PERCENT
+    assert alignment.offset == 0.2
+
+
+@pytest.mark.unit
+def test_alignment_from_alignment_object():
+    original_alignment = Alignment(
+        direction=AlignmentDirection.RIGHT, offset_type=OffsetType.UNIT, offset=10
+    )
+    new_alignment = Alignment.from_value(original_alignment)
+    assert new_alignment.direction == AlignmentDirection.RIGHT
+    assert new_alignment.offset_type == OffsetType.UNIT
+    assert new_alignment.offset == 10
+
+
+@pytest.mark.unit
+def test_as_tuple():
+    alignment = Alignment(
+        direction=AlignmentDirection.RIGHT, offset_type=OffsetType.UNIT, offset=15
+    )
+    assert alignment.as_tuple() == (AlignmentDirection.RIGHT, OffsetType.UNIT, 15)
+
+
+@pytest.mark.unit
+def test_percent_of():
+    alignment = Alignment(offset_type=OffsetType.PERCENT, offset=0.5)
+    assert alignment.percent_of(100) == 50
+
+    alignment = Alignment(offset_type=OffsetType.UNIT, offset=30)
+    with pytest.raises(ValueError):
+        alignment.percent_of(100)
+
+
+@pytest.mark.unit
+def test_invalid_direction():
+    with pytest.raises(ValueError):
+        Alignment.from_value("widdershins:50")
+
+
+@pytest.mark.unit
+def test_invalid_type():
+    with pytest.raises(ValueError):
+        Alignment.from_value(1)
+
+@pytest.mark.unit
+def test_invalid_center_with_offset():
+    with pytest.raises(ValueError):
+        Alignment(direction=AlignmentDirection.CENTER, offset=100)
+
+@pytest.mark.unit
+def test_invalid_center_with_offset_from_string():
+    with pytest.raises(ValueError):
+        Alignment.from_string("center:80%")
+
+
+@pytest.mark.unit
+def test_str_method():
+    alignment = Alignment(
+        direction=AlignmentDirection.LEFT, offset_type=OffsetType.PERCENT, offset=0.25
+    )
+    assert str(alignment) == "left:25.0%"
+
+    alignment = Alignment(
+        direction=AlignmentDirection.RIGHT, offset_type=OffsetType.UNIT, offset=10
+    )
+    assert str(alignment) == "right:10"
